5fe8852d8f2fa51e09294fdce3e66fd2b28cccf5
alex koe <alex.koe@binfinite.com.my>
2021-11-20 00:55:43 +0800

    delete useless coode, use button_to to submit for delete instead of link_to which made 500 error in heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-	rescue_from AbstractController::ActionNotFound, with: :not_found
-
-	def not_found
-		render text: 'Action not found'
-	end
 end
