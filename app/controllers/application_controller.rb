cd7ef044204ec90625a4a92f4ade5baae534acd5
alex koe <alex.koe@binfinite.com.my>
2021-11-20 00:12:49 +0800

    try to fix jquery not working on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+	rescue_from AbstractController::ActionNotFound, with: :not_found
+
+	def not_found
+		render text: 'Action not found'
+	end
 end
