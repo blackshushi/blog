a5ca5eaf7481363dd3cae77a17b0a6ed1d05febe
alex koe <alex.koe@binfinite.com.my>
2021-11-21 00:23:05 +0800

    delete unuse code and remove class for select-field as doesn't define in any

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,5 @@
 class Comment < ApplicationRecord
   include Visible
   belongs_to :article
-  before_create :assign_status
-
-  private
-  def assign_status
-    self.status = 'public'
-  end
 end
 
